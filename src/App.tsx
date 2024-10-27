68d2d1c0326a7db0e03377efde866f62f9666c78
Roma Bulani <roma.bulani@vista.com>
2024-10-28 01:08:55 +0530

    refactor: added scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import Header from "./components/molecules/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/atoms/Footer";
+import ScrollToTop from "./components/atoms/ScrollToTop";
 
 function App() {
   return (
     <div className="flex flex-col max-w-[1024px] mx-auto">
+      <ScrollToTop />
       <Header />
       <div className="min-h-[calc(100vh-130px)] mx-auto">
         <Outlet />
